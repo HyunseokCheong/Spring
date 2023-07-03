2dfcf2372384d3aff78df23a21a34869660d49fe
hyunseokcheong <hyunseokcheong@gmail.com>
2023-07-03 21:47:28 +0900

    first commit

---
+++ oauth2-jwt-security/src/main/java/com/hyunseokcheong/oauth2jwtsecurity/Oauth2JwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.hyunseokcheong.oauth2jwtsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtSecurityApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2JwtSecurityApplication.class, args);
+    }
+    
+}
---
+++ oauth2-jwt-security/src/test/java/com/hyunseokcheong/oauth2jwtsecurity/Oauth2JwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunseokcheong.oauth2jwtsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2JwtSecurityApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
